745e6ed730d592bb7f22bb64a31367cc42ff9ce8
Ziiron1 <guilherme_profissional@yahoo.com.br>
2022-12-05 00:59:41 -0300

    Featured Movie
    -Updating Api Movies
    -Getting Principal Movie Info

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Tmdb from './Tmdb'
-import MovieRow from './components/MovieRow';
-
+import MovieRow from './components/MovieRow/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie/FeaturedMovie';
 
 /* Style */
 import './App.css'
@@ -11,6 +11,7 @@ import './App.css'
 function App() {
 
   const [movielist, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
 
   useEffect(() => {
     const loadAll = async () => {
@@ -20,6 +21,12 @@ function App() {
       setMovieList(list);
       console.log(list);
 
+      /* Pegando Feature */
+      let originals = list.filter(i => i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
     }
 
     loadAll();
@@ -27,6 +34,11 @@ function App() {
 
   return (
     <div className='page'>
+
+      {featuredData &&
+        <FeaturedMovie item={featuredData} />
+      }
+
       <section className='lists'>
         {movielist.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
--- src/Tmdb.jsx
+++ src/Tmdb.jsx
@@ -24,44 +24,43 @@ export default {
             {
                 slug: 'originals',
                 title: 'Originais do Netflix',
-                items: await basicFetch(`/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/discover/tv?api_key=${API_KEY}&with_networks=213&language=pt-BR`)
             },
             {
                 slug: 'trending',
                 title: 'Recomendados para Você',
-                items: await basicFetch(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/trending/all/week?api_key=${API_KEY}&language=pt-BR`)
             },
             {
                 slug: 'toprated',
                 title: 'Em alta',
-                items: await basicFetch(`/movie/top_rated?language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/trending/all/week?api_key=${API_KEY}&language=pt-BR&page=5`)
             },
             {
                 slug: 'action',
                 title: 'Ação',
-                items: await basicFetch(`/discover/movie?with_genres=28language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/discover/movie?api_key=${API_KEY}&with_genres=28&language=pt-BR&page=2`)
             },
             {
                 slug: 'comedy',
                 title: 'Comédia',
-                items: await basicFetch(`/discover/movie?with_genres=35language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/discover/movie?api_key=${API_KEY}&with_genres=35&language=pt-BR&page=9`)
             },
             {
                 slug: 'horror',
                 title: 'Terror',
-                items: await basicFetch(`/discover/movie?with_genres=27language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/discover/movie?language=pt-BR&api_key=${API_KEY}&with_genres=27&language=pt-BR&page=3`)
             },
             {
                 slug: 'romance',
                 title: 'Romance',
-                items: await basicFetch(`/discover/movie?with_genres=10749language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/discover/movie?api_key=${API_KEY}&with_genres=10749&language=pt-BR&page=18`)
             },
             {
                 slug: 'documentary',
                 title: 'Documentário',
-                items: await basicFetch(`/discover/movie?with_genres=99language=pt-BR&api_key=${API_KEY}`),
+                items: await basicFetch(`/discover/movie?api_key=${API_KEY}&with_genres=99&language=pt-BR`),
             },
-
             {
                 slug: 'upcoming',
                 title: 'Recém Chegados',
@@ -70,14 +69,57 @@ export default {
             {
                 slug: 'voteaverage',
                 title: 'Mais votados',
-                items: await basicFetch(`/movie/top_rated?sort_by=popularity.desc&api_key=3aa2b05e0b056282256b25c9224ce1c2&language=pt-BR&page=1`)
+                items: await basicFetch(`/movie/top_rated?sort_by=popularity.desc&api_key=3aa2b05e0b056282256b25c9224ce1c2&language=pt-BR&page=3`)
             },
-        ]
+            {
+                slug: 'constructor',
+                title: 'Seriados sobre Construção',
+                items: await basicFetch(`/discover/tv?api_key=${API_KEY}&with_networks=210&language=pt-BR`)
+            },
+            {
+                slug: 'anime',
+                title: 'Animes',
+                items: await basicFetch(`/discover/tv?api_key=${API_KEY}&with_networks=1&language=pt-BR`)
+            },
+            {
+                slug: 'desenho',
+                title: 'Seriados Jovens',
+                items: await basicFetch(`/discover/tv?api_key=${API_KEY}&with_networks=13&language=pt-BR`)
+            },
+            {
+                slug: 'adult',
+                title: 'Para Adultos',
+                items: await basicFetch(`/discover/movie?api_key=${API_KEY}&with_genres=10749&language=pt-BR&page=1`)
+            },
+        ];
+    },
+   getMovieInfo: async(movieId, type) => {
+        let info = {};
+    
+        if(movieId) {
+            switch(type){
+                case 'movie':
+                info = await basicFetch(`/movie/${movieId}?language=pt-BR&api_key=${API_KEY}`);
+                break;
+                case 'tv':
+                    info = await basicFetch(`/tv/${movieId}?language=pt-BR&api_key=${API_KEY}`);
+                break;
+                default:
+                    info = null;
+                    break;
+            }
+        }
+    
+        return info;
     }
-
+    
 }
             // {
             //     slug: 'SIMILAR',
             //     title: 'SIMILARES',
             //     items: await basicFetch(`/movie/${movie_id}/similar?api_key=3aa2b05e0b056282256b25c9224ce1c2&language=pt-BR&page=1`),
             // },
+
+        
+            // Colocar um botao descritivo na info do filme como (Ver similares / recomendações parecidas com este filme 
+            // ai poe o link tipo https://api.themoviedb.org/3/movie/{movie_ID}/recommendations?api_key=3aa2b05e0b056282256b25c9224ce1c2&language=pt-BR&page=1)
